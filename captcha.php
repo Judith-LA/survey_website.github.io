4b00814171c833ebff94e273293083a6a539ef93
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-03-30 19:01:42 +0200

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,36 +1,22 @@
 <?php
-if (isset($_POST['g-recaptcha-response'])) {
-    $captcha = $_POST['g-recaptcha-response'];
-} else {
-    $captcha = false;
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
 
-if (!$captcha) {
-    console.log("No Captcha!");
-} else {
-    $secret   = '6LfnbZUaAAAAAL_ZihPrFoTALeyD0dPzb2TkWGn3';
-    $response = file_get_contents(
-        "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']
-    );
-    // use json_decode to extract json response
-    $response = json_decode($response);
+    // Build POST request:
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6LfnbZUaAAAAAL_ZihPrFoTALeyD0dPzb2TkWGn3';
+    $recaptcha_response = $_POST['recaptcha_response'];
 
-    //console.log($response->score)
+    // Make and decode POST request:
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+    
+    echo $recaptcha;
 
-    //if ($response->success === false) {
-    //    console.log("Captcha error!");
+    // Take action based on the score returned:
+    //if ($recaptcha->score >= 0.5) {
+        // Verified - send email
+    //} else {
+        // Not verified - show form error
     //}
 
-    if($response["success"]) {
-        echo json_encode(array('success' => 'true'));
-    } else {
-        echo json_encode(array('success' => 'false'));
-    }
-}
-
-//... The Captcha is valid you can continue with the rest of your code
-//... Add code to filter access using $response . score
-//if ($response->success==true && $response->score <= 0.5) {
-    //Do something to denied access
-//}
-?>
+} ?>
