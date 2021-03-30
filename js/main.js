c39faa442c9e6eb4aa806bc8f00287e0f424883a
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-03-30 13:01:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ function nextQuestion(){
 		document.getElementById('next').style.display = 'none';
 		document.getElementById('end_survey').style.display = 'block';
 	}
-	$('input[name=Options]:checked').removeAttr('checked');
+	$('input[name="Options"]:checked').removeAttr('checked');
 	i = i+1;
 	add_comment(i);
 }
